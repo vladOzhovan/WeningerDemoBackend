240d455258ec95c9da3dbb106f02f6baa7895e29
Vladimir <vovchik.v.kf@gmail.com>
2025-06-15 00:45:11 +0200

    Local migrations

---
+++ Data/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WeningerDemoProject.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Local connection string
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=weninger_local;Username=postgres;Password=your_password");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
